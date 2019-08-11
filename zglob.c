f78785ffeb9a7eefe2d01a729d3d8769e7e9f513
Adam D. Moss <c@yotes.com>
2019-08-10 22:23:39 -0700

    actually really add escaped wildcard support.  d'oh.  but on a branch because honestly nobody asked for this...

--- zglob.c
+++ zglob.c
@@ -1,29 +1,17 @@
 /* ZGlob: Utilities for easy matching of strings using * and ? wildcards.
-   Adam D. Moss <adam@gimp.org> <aspirin@icculus.org>
 
-Copyright (C) 2004 Adam D. Moss (the "Author").  All Rights Reserved.
+Copyright (C) 2004-2019 by Adam D. Moss <c@yotes.com>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is fur-
-nished to do so, subject to the following conditions:
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FIT-
-NESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CON-
-NECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-Except as contained in this notice, the name of the Author of the
-Software shall not be used in advertising or otherwise to promote the sale,
-use or other dealings in this Software without prior written authorization
-from the Author.
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 #include <string.h>
 #include "zglob.h"
@@ -74,14 +62,18 @@ zglob(const char *const str,
   char wildc;
   zdprintf(" [%s:%s]", str, wildcard);
   while ((wildc = wildcard[wildpos]) != '\0') {
-    if (wildc == '?') {
+  	int use_as_literal = (wildc == '\\'); /*escaped*/
+  	if (use_as_literal) wildc = wildcard[++wildpos];
+  	/* a trailing '\' in wildcard is treated as a literal \ rather than an escape' */
+  	if (wildc == '\0') return ('\\' == str[stroff] && '\0' == str[stroff+1]);
+    if (wildc == '?' && !use_as_literal) {
       /* happily eat any char except end-of-string */
       if (str[stroff] == '\0') {
         zdprintf("(?eos)");
         return 0;
       }
       ++stroff;
-    } else if (wildc == '*') {
+    } else if (wildc == '*' && !use_as_literal) {
       int litlen;
       while (wildcard[wildpos+1] == '*' ||
              wildcard[wildpos+1] == '?') {
@@ -96,6 +88,7 @@ zglob(const char *const str,
         /* compound multiple '*' */
         ++wildpos;
       }
+
       if (wildcard[wildpos+1] == '\0') {
         return 1; /* '*' at end of wildcard matches remainder of str */
       }
@@ -107,9 +100,10 @@ zglob(const char *const str,
              wildcard[wildpos + 1 + litlen] != '?') {
         ++litlen;
       }
+
       if (litlen) {
         /* look for each successive whole match in str after stroff,
-           recurse into each one! */
+           recurse into each one */
         int foundoff = zglob_strloc(&str[stroff],
                                     &wildcard[wildpos + 1],
                                     litlen);
@@ -182,6 +176,44 @@ main(void) {
   TESTRESULT("a", "a**", 1);
   TESTRESULT("a", "**a**", 1);
 
+  TESTRESULT("", "\\", 0);
+  TESTRESULT("\\", "\\", 1);
+  TESTRESULT("a\\", "\\", 0);
+  TESTRESULT("a\\", "a\\", 1);
+  TESTRESULT("\\", "a\\", 0);
+  TESTRESULT("\\", "", 0);
+
+  TESTRESULT("*", "*", 1);
+  TESTRESULT("a", "\\*", 0);
+  TESTRESULT("*", "\\*", 1);
+  TESTRESULT("a", "a\\*", 0);
+  TESTRESULT("a", "\\*a", 0);
+
+  TESTRESULT("?", "?", 1);
+  TESTRESULT("?", "\\?", 1);
+  TESTRESULT("?", "*", 1);
+  TESTRESULT("?", "\\*", 0);
+  TESTRESULT("\\?", "?", 0);
+  TESTRESULT("\\?", "\\?", 0);
+  TESTRESULT("\\?", "\\\\?", 1);
+  TESTRESULT("\\?", "*", 1);
+  TESTRESULT("\\?", "\\*", 0);
+  TESTRESULT("*", "?", 1);
+
+  TESTRESULT("a", "\\a", 1);
+  TESTRESULT("b", "\\a", 0);
+  TESTRESULT("a\\*", "a\\*", 0);
+  TESTRESULT("a*", "a*", 1);
+  TESTRESULT("a*", "a\\*", 1);
+  TESTRESULT("a\\*", "a\\\\*", 1);
+
+  TESTRESULT("\\a", "\\\\a", 1);
+  TESTRESULT("\\", "\\\\", 1);
+  TESTRESULT("\\", "\\\\\\", 0);
+  TESTRESULT("\\\\", "\\\\\\\\", 1);
+  TESTRESULT("\\\\", "\\\\*", 1);
+  TESTRESULT("\\\\", "*\\\\", 1);
+
   TESTRESULT("abcd", "abcd", 1);
   TESTRESULT("abcd", "*bcd", 1);
   TESTRESULT("abcd", "a*cd", 1);
@@ -199,10 +231,15 @@ main(void) {
   TESTRESULT("abcd", "ab*", 1);
   TESTRESULT("abcd", "*abcd", 1);
   TESTRESULT("abcd", "abcd*", 1);
+
   TESTRESULT("", "", 1);
+  TESTRESULT("", "*", 1);
 
   TESTRESULT("abcd", "abc", 0);
   TESTRESULT("abcd", "*dd", 0);
+  TESTRESULT("abcd", "???dd", 0);
+  TESTRESULT("abcd", "aa*", 0);
+  TESTRESULT("abcd", "aa???", 0);
   TESTRESULT("abcd", "bcd", 0);
   TESTRESULT("", "a", 0);
   TESTRESULT("a", "", 0);
@@ -251,6 +288,7 @@ main(void) {
   TESTRESULT("abbxbbcbb", "a*b*bb", 1);
   TESTRESULT("abbxbbcbb", "a*bb*bb", 1);
   TESTRESULT("a", "*?*", 1);
+
   printf("\n%d tests passed, %d tests failed\n", successes, failures);
   if (failures)
   {
--- zglob.h
+++ zglob.h
@@ -1,36 +1,24 @@
 /* ZGlob: Utilities for easy matching of strings using * and ? wildcards.
-   Adam D. Moss <adam@gimp.org> <aspirin@icculus.org>
 
-Copyright (C) 2004 Adam D. Moss (the "Author").  All Rights Reserved.
+Copyright (C) 2004-2019 by Adam D. Moss <c@yotes.com>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is fur-
-nished to do so, subject to the following conditions:
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FIT-
-NESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CON-
-NECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-Except as contained in this notice, the name of the Author of the
-Software shall not be used in advertising or otherwise to promote the sale,
-use or other dealings in this Software without prior written authorization
-from the Author.
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 #ifndef _Z_GLOB_H
 #define _Z_GLOB_H
 
-/* note: '*' matches 0 or more times, '?' matches exactly once.
-   escaping rules not implemented (yet).  function does the right
-   thing with multiple simultaneous wildcards. */
+/* wildcard note: '*' matches 0 or more times, '?' matches exactly once.
+   '\' escapes any character in wildcard string so '?' and '*' can be used as
+   literal matches, i.e. zglob("ab","\\?b")==0 */
 
 /* Returns 1 if 'str' matches 'wildcard', otherwise 0. */
 int zglob(const char *const str,
